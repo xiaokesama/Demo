a2d55ec7df31ef4f74dbcc150d20bde5479fab26
xiaokesama <1193274986@qq.com>
2024-09-21 15:49:17 +0800

    message

--- auto_admin/content.js
+++ auto_admin/content.js
@@ -23,7 +23,7 @@ function isUrlMatching(pattern, url) {
     UrlUsrPwdArray.forEach(UrlUsrPwd => {
         if(UrlUsrPwd && UrlUsrPwd.matchUrl && isUrlMatching(UrlUsrPwd.matchUrl, window.location.href)){
             console.log('匹配到URL：' + window.location.href)
-            const checkElement = setInterval(() => {
+            const checkElement = setInterval(() => {  //setInterval 用于按照指定的时间间隔重复执行一个指定的函数
                 usernameElement = document.querySelector(UrlUsrPwd.usernameSelector);
                 passwordElement = document.querySelector(UrlUsrPwd.passwordSelector);
                 submitButtonElement = document.querySelector(UrlUsrPwd.submitButtonSelector);
@@ -40,7 +40,7 @@ function isUrlMatching(pattern, url) {
                     clearInterval(checkElement);
                 }
                 if (submitButtonElement && UrlUsrPwd.toggleSwitch) {
-                    setTimeout(() => {
+                    setTimeout(() => {   //setTimeout 用于在指定的延迟时间后执行一次指定的函数
                         console.log("等待 0.1 秒");
                     }, 100);
                     console.log('点击提交按钮')
